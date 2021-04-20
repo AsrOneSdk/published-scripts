83d3da033caacadb6db69cea3892f6af2334a1d6
ayfathim <ayfathim@microsoft.com>
2021-04-20 12:04:59 +0530

    Added support for government clouds as well.

--- Cleanup-Stale-ASR-Config-Azure-VM.ps1
+++ Cleanup-Stale-ASR-Config-Azure-VM.ps1
@@ -1,36 +1,55 @@
-cls
+Param
+(
+    [Parameter( `
+        Mandatory=$true, `
+        HelpMessage = 'Enter subscription Id to be used.')]
+        [string]$subscriptionId,
+    [Parameter( `
+        Mandatory=$true, `
+        HelpMessage = 'Specify the name of the Resource group in which your VM is located.')]
+        [string]$rgName,
+    [Parameter( `
+        Mandatory=$true, `
+        HelpMessage = 'Specify the name of the VM.')]
+        [string]$vmName,
+    [Parameter( `
+        Mandatory=$false, `
+        HelpMessage = 'The login environmnet to be used. The default value is AzureCloud for public clouds. For Givernemnt clouds, specify as AzureUSGovernment. For more details about Government clouds, refer:  https://docs.microsoft.com/en-us/azure/azure-government/documentation-government-get-started-connect-with-ps')]
+        [string]$loginEnvironment ="AzureCloud"
+)
 
-$subscriptionId = Read-Host 'What is your Azure Subscription ID?'
-$rgName = Read-Host 'Specify the name of the Resource group in which your VM is located'
-$vmName = Read-Host 'Specify the name of the VM'
 
 # Sign-in with Azure account credentials
-Login-AzureRmAccount
+Login-AzAccount -Environment $loginEnvironment 
 
-# Select Azure Subscription
-Select-AzureRmSubscription -SubscriptionId $subscriptionId
+#Select Azure Subscription
+Select-AzSubscription -SubscriptionId $subscriptionId
 
 # Remove any locks t
-$locks = Get-AzureRmResourceLock -ResourceGroupName $rgName -ResourceName $vmName -ResourceType Microsoft.Compute/virtualMachines
+$locks = Get-AzResourceLock -ResourceGroupName $rgName -ResourceName $vmName -ResourceType Microsoft.Compute/virtualMachines
 if ($locks -ne $null -and $locks.Count -ge 0){
 	$canDelete =  Read-Host 'The VM has locks that could prevent cleanup of Azure Site Recovery stale links left from previous protection. Do you want the locks deleted to ensure cleanup goes smoothly? Reply with Y/N.'
 	
 	if ($canDelete.ToLower() -eq "y"){
 		Foreach ($lock in $locks) {
 			$lockId = $lock.LockId
-			Remove-AzureRmResourceLock -LockId $lockId -Force
+			Remove-AzResourceLock -LockId $lockId -Force
 			Write-Host "Removed Lock $lockId for $vmName"
 		}	
 	}
 }
 
-$linksResourceId = 'https://management.azure.com/subscriptions/' + $subscriptionId  + '/providers/Microsoft.Resources/links'
+$context = Get-AzContext
+
+$azureUrl = $context.Environment.ResourceManagerUrl
+
+$linksResourceId = $azureUrl + "subscriptions/" + $subscriptionId  + '/providers/Microsoft.Resources/links'
 $vmId = '/subscriptions/' + $subscriptionId + '/resourceGroups/' + $rgName + '/providers/Microsoft.Compute/virtualMachines/' + $vmName + '/'
 
 Write-Host $("Deleting links for $vmId using resourceId: $linksResourceId")
  
 
-$links = @(Get-AzureRmResource -ResourceId $linksResourceId|  Where-Object {$_.Properties.sourceId -match $vmId -and $_.Properties.targetId.ToLower().Contains("microsoft.recoveryservices/vaults")})
+$links = @(Get-AzResource -ResourceId $linksResourceId|  Where-Object {$_.Properties.sourceId -match $vmId -and $_.Properties.targetId.ToLower().Contains("microsoft.recoveryservices/vaults")})
 Write-Host "Links to be deleted"
 $links
 
@@ -39,11 +58,11 @@ Foreach ($link in $links)
 
 {
  Write-Host $("Deleting link " + $link.Name)
- Remove-AzureRmResource -ResourceId $link.ResourceId -Force
+ Remove-AzResource -ResourceId $link.ResourceId -Force
 }
 
 
-$links = @(Get-AzureRmResource -ResourceId $linksResourceId|  Where-Object {$_.Properties.sourceId -match $vmId -and $_.Properties.targetId.ToLower().Contains("/protecteditemarmid/")})
+$links = @(Get-AzResource -ResourceId $linksResourceId|  Where-Object {$_.Properties.sourceId -match $vmId -and $_.Properties.targetId.ToLower().Contains("/protecteditemarmid/")})
 Write-Host "Cross subscription Links to be deleted"
 $links
 
@@ -53,7 +72,7 @@ Foreach ($link in $links)
 
 {
  Write-Host $("Deleting link " + $link.Name)
- Remove-AzureRmResource -ResourceId $link.ResourceId -Force
+ Remove-AzResource -ResourceId $link.ResourceId -Force
 }
  
 Write-Host $("Deleted all links ")
