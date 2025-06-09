6d2ee463543fbfca9cc32283086e6ce954405a7c
Shashank Agarwal <shasagarwal@microsoft.com>
2025-06-09 18:45:02 +0530

    Add Get-PlainTextAzAccessToken function for retrieving plain text Azure access tokens

--- CreateResourceLinkForProtectedItem.ps1
+++ CreateResourceLinkForProtectedItem.ps1
@@ -30,6 +30,60 @@ Param
         [string]$loginEnvironment ="AzureCloud"
 )
 
+### <summary>
+### Retrieves an Azure access token in plain text format for a specified resource or tenant.
+### This function handles secure tokens and converts them to plain text for easier usage in scenarios
+### where plain text tokens are required.
+### </summary>
+### <param name="ResourceUrl">Optional parameter specifying the resource URL for which the token is requested.</param>
+### <param name="ResourceTypeName">Optional parameter specifying the resource type name for which the token is requested.</param>
+### <param name="TenantId">Optional parameter specifying the tenant ID for which the token is requested.</param>
+### <return>
+### Returns an object containing the access token in plain text format and other token details.
+### </return>
+function Get-PlainTextAzAccessToken {
+    [CmdletBinding(DefaultParameterSetName = 'Default')]
+    param (
+        [Parameter(ParameterSetName = 'ByResourceUrl')]
+        [string]$ResourceUrl,
+
+        [Parameter(ParameterSetName = 'ByResourceTypeName')]
+        [string]$ResourceTypeName,
+
+        [Parameter(ParameterSetName = 'ByTenantId')]
+        [string]$TenantId
+    )
+
+    # Build parameter dictionary dynamically
+    $params = @{}
+    if ($PSCmdlet.ParameterSetName -eq 'ByResourceUrl') {
+        $params['ResourceUrl'] = $ResourceUrl
+    }
+    elseif ($PSCmdlet.ParameterSetName -eq 'ByResourceTypeName') {
+        $params['ResourceTypeName'] = $ResourceTypeName
+    }
+    elseif ($PSCmdlet.ParameterSetName -eq 'ByTenantId') {
+        $params['TenantId'] = $TenantId
+    }
+
+    $tokenResponse = Get-AzAccessToken @params
+
+    # Handle SecureString token
+    if ($tokenResponse.Token -is [System.Security.SecureString]) {
+        $ptr = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($tokenResponse.Token)
+        try {
+            $plainToken = [System.Runtime.InteropServices.Marshal]::PtrToStringBSTR($ptr)
+        }
+        finally {
+            [System.Runtime.InteropServices.Marshal]::ZeroFreeBSTR($ptr)
+        }
+
+        # Replace the Token property with plain text
+        $tokenResponse | Add-Member -MemberType NoteProperty -Name Token -Value $plainToken -Force
+    }
+
+    return $tokenResponse
+}
 
 # Sign-in with Azure account credentials
 Login-AzAccount -Environment $loginEnvironment 
@@ -70,7 +124,7 @@ $resourceLinkSourceId
 $resourceLinkName  
 
 # Trigger creation of resource link.
-$token = (Get-AzAccessToken).Token
+$token = (Get-PlainTextAzAccessToken).Token
 $Header = @{"authorization" = "Bearer $token"}
 $Header['Content-Type'] = "application\json"
 
